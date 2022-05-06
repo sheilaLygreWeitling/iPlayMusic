ef2202dae467352547ec0e9c044aab364e88767c
MarcusHjorth <U773559@rts-365.dk>
2022-05-06 14:12:59 +0200

    Color Scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'darkBlue': '#114793',
+        'lightBlue': '#00A1CB',
+        'turkisBlue': '#0ABEBE',
+        'darkGreen': '#3A7634',
+        'green': '#5EB11C',
+        'yellow': '#F2BC06',
+        'orange': '#F18D05',
+        'darkPink': '#D70060',
+        'red': '#E54028',
+        'pink': '#FF1168',
+        'white': '#FFFFFF',
+        'darkPurple': '#341931',
+        'black': '#111625',
+        
+        'gradientGreenFrom': '#5EB11C',
+        'gradientGreenTo': '#3A7634',
+        
+        'gradientBlueFrom': '#00A1CB',
+        'gradientBlueTo': '#115793',
+        
+        'gradientRedFrom': '#EE0979',
+        'gradientRedTo': '#3FF6A00',
+        
+        'reallyBlack': '#111111'
+      },
+    },
   },
   plugins: [],
 }
