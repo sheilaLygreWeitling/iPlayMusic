3cc5db475cc3df553a98b2b228fcbb9efa8e0b11
Dragonashes <dlsfs@tutanota.de>
2022-05-09 13:30:01 +0200

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import Page1 from './pages/Page1'
+import Page2 from './pages/Page2'
 
 function App() {
   return (
-    <>  </>
-  );
+    <>
+      <div>navbar</div>
+
+      <a href="/">Home</a>
+      <a href="Page1">Page1</a>
+      <a href="Page2">Page2</a>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Page1" element={<Page1 />} />
+        <Route path="/Page2" element={<Page2 />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
