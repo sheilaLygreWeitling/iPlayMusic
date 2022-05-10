32898815ed0de040a7c759155ec31d8e89802f6e
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-05-10 12:13:51 +0200

    Add back, page and search til header in NowPlaying branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+import NowPlaying from "./pages/Heading/NowPlaying";
 import Home from "./pages/Home";
+
 import Page1 from "./pages/Page1";
-import Page2 from "./pages/Page2";
+
 
 function App() {
   return (
@@ -10,12 +12,12 @@ function App() {
 
       <a href="/">Home</a>
       <a href="Page1">Page1</a>
-      <a href="Page2">Page2</a>
+      <a href="NowPlaying">Page2</a>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Page1" element={<Page1 />} />
-        <Route path="/Page2" element={<Page2 />} />
+        <Route path="NowPlaying" element={<NowPlaying />} />
       </Routes>
     </>
   );
---
+++ src/assets/components/Heading/Playing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Playing = () => {
+    return (
+        <div>
+            Playing
+        </div>
+    )
+}
+
+export default Playing
