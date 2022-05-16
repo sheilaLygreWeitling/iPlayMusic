76d5e888ebe70c3fd497f415bb95ed33fff27b20
Dragonashes <dlsfs@tutanota.de>
2022-05-16 12:44:30 +0200

    add token check and format code

--- src/Components/Loadsongs.js
+++ src/Components/Loadsongs.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Loadsongs = () => {
-
   const [token, setToken] = useState("");
 
   const [tracksarray, setArtists] = useState([]);
@@ -24,41 +23,45 @@ const Loadsongs = () => {
 
     setToken(token);
 
-    const InitialSongs = async () => {
-      // https://api.spotify.com/v1/me/tracks?limit=50
-      // prøv SPOTIFY me tracks
+    if (token) {
+      const InitialSongs = async () => {
+        // https://api.spotify.com/v1/me/tracks?limit=50
+        // prøv SPOTIFY me tracks
 
-      var albumids = "382ObEPsp2rxGrnsizN5TX,1A2GTWGtFfWp7KSQTwWOyo,2noRn2Aes5aoNVsU6iWThc,4aawyAB9vmqN3uQ7FjRGTy"
-      // https://api.spotify.com/v1/albums/{id}
-      const { data } = await axios.get(`https://api.spotify.com/v1/albums?ids=${albumids}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        }
-      });
-      // console.log(data.tracks)
+        var albumids =
+          "382ObEPsp2rxGrnsizN5TX,1A2GTWGtFfWp7KSQTwWOyo,2noRn2Aes5aoNVsU6iWThc,4aawyAB9vmqN3uQ7FjRGTy";
+        // https://api.spotify.com/v1/albums/{id}
+        const { data } = await axios.get(
+          `https://api.spotify.com/v1/albums?ids=${albumids}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        // console.log(data.tracks)
 
-      const testarray = data.albums
-      console.log(testarray)
-      // console.log(data.tracks[0])
-      // console.log(data.tracks[1])
-      // console.log(data.tracks[2])
-      // console.log(data.tracks[2])
-      // console.log(data.tracks[0].album)
-      // data.tracks.forEach(item => {
+        const testarray = data.albums;
+        console.log(testarray);
+        // console.log(data.tracks[0])
+        // console.log(data.tracks[1])
+        // console.log(data.tracks[2])
+        // console.log(data.tracks[2])
+        // console.log(data.tracks[0].album)
+        // data.tracks.forEach(item => {
         // console.log(item.name)
         // console.log(item.id)
-      // });
+        // });
 
-      setArtists(data.albums);
-    };
+        setArtists(data.albums);
+      };
 
-
-    InitialSongs();
+      InitialSongs();
+    }
 
   }, []);
 
   const renderSongs = () => {
-
     // see console log for more
     return tracksarray.map((albums, index) => (
       <div key={index}>
@@ -74,16 +77,11 @@ const Loadsongs = () => {
     <header className="App-header">
       <h1>Load songs</h1>
 
-      {!token ? (
-        <div>normal page content</div>
-      ) : (
-        <div>search page content</div>
-      )}
+      {!token ? <div>normal page content</div> : <div>search page content</div>}
 
       {renderSongs()}
-
     </header>
   );
-}
+};
 
 export default Loadsongs;
