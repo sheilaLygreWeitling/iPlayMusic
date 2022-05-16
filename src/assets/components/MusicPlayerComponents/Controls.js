969492c6281fc287445abf8482253e833f0f0c3e
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-05-16 13:33:51 +0200

    Add gradient color to play/pause button

--- src/assets/components/MusicPlayerComponents/Controls.js
+++ src/assets/components/MusicPlayerComponents/Controls.js
@@ -6,21 +6,30 @@ const Controls = (props) => {
 
     return (
         <div>
-            <button className='musicPlayer-btn skipBackward-btn' onClick={() => props.SkipSong(false)} >
-                <AiFillStepBackward />
-            </button>
-            <button className='musicPlayer-btn backwards-btn'>
-                <AiOutlineBackward />
-            </button>
-            <button className='musicPlayer-btn play-btn' onClick={() => props.setIsPlaying(!props.isPlaying)}>
-                {props.isPlaying ? <BsPauseFill /> : <BsFillPlayFill />}
-            </button>
-            <button className='musicPlayer-btn forward-btn'>
-                <AiFillForward />
-            </button>
-            <button className='musicPlayer-btn skipForward-btn' onClick={() => props.SkipSong()}  >
-                <AiFillStepForward />
-            </button>
+            <div className="flex gap-[20px]">
+                <button className='musicPlayer-btn skipBackward-btn text-white' onClick={() => props.SkipSong(false)} >
+                    <AiFillStepBackward />
+                </button>
+                <button className='musicPlayer-btn backwards-btn text-white'>
+                    <AiOutlineBackward />
+                </button>
+                <button className='musicPlayer-btn play-btn text-[100px]' onClick={() => props.setIsPlaying(!props.isPlaying)}>
+                    <svg width="0" height="0">
+                        {/*LINEAR GRADIENT ON REACT-ICON*/}
+                        <linearGradient id="a" x1="100%" y1="100%" x2="0" y2="0">
+                            <stop stopColor="#EE0979" offset="0%" />
+                            <stop stopColor="#FF6A00" offset="100%" />
+                        </linearGradient>
+                    </svg>
+                    {props.isPlaying ? <BsPauseFill style={{ fill: "url(#a)" }} /> : <BsFillPlayFill style={{ fill: "url(#a)" }} />}
+                </button>
+                <button className='musicPlayer-btn forward-btn text-white'>
+                    <AiFillForward />
+                </button>
+                <button className='musicPlayer-btn skipForward-btn text-white' onClick={() => props.SkipSong()}  >
+                    <AiFillStepForward />
+                </button>
+            </div>
         </div>
     )
 }
