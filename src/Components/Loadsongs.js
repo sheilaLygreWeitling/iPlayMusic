9fc4df0367add395853eef61d0cd03217c107ad8
Dragonashes <dlsfs@tutanota.de>
2022-05-16 13:01:34 +0200

    add login check

--- src/Components/Loadsongs.js
+++ src/Components/Loadsongs.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Loadsongs = () => {
-
   const [token, setToken] = useState("");
 
   useEffect(() => {
@@ -22,28 +21,33 @@ const Loadsongs = () => {
 
     setToken(token);
 
-    const InitialSongs = async (e) => {
-      // e.preventDefault();
-      // ed sheeran
-      // https://api.spotify.com/v1/track/2UN0lp72LAusrXi8LLVomt
-      // spotify example
-      // https://api.spotify.com/v1/tracks/2KrxsD86ARO5beq7Q0Drfqa
-      // https://api.spotify.com/v1/search?query=Testshot&type=artist&locale=da-DK%2Cda%3Bq%3D0.9%2Cen-US%3Bq%3D0.8%2Cen%3Bq%3D0.7&offset=0&limit=20
-      const { data } = await axios.get("https://api.spotify.com/v1/search?query=Testshot&type=artist&locale=da-DK%2Cda%3Bq%3D0.9%2Cen-US%3Bq%3D0.8%2Cen%3Bq%3D0.7&offset=0&limit=20", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        params: {
-          type: "artist",
-        },
-      });
+    if (token) {
+      const InitialSongs = async (e) => {
+        // e.preventDefault();
+        // ed sheeran
+        // https://api.spotify.com/v1/track/2UN0lp72LAusrXi8LLVomt
+        // spotify example
+        // https://api.spotify.com/v1/tracks/2KrxsD86ARO5beq7Q0Drfqa
+        // https://api.spotify.com/v1/search?query=Testshot&type=artist&locale=da-DK%2Cda%3Bq%3D0.9%2Cen-US%3Bq%3D0.8%2Cen%3Bq%3D0.7&offset=0&limit=20
+        const { data } = await axios.get(
+          "https://api.spotify.com/v1/search?query=Testshot&type=artist&locale=da-DK%2Cda%3Bq%3D0.9%2Cen-US%3Bq%3D0.8%2Cen%3Bq%3D0.7&offset=0&limit=20",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            params: {
+              type: "artist",
+            },
+          }
+        );
 
-      console.log(data)
+        console.log(data);
 
-      setArtists(data.artists.items);
-    };
+        setArtists(data.artists.items);
+      };
 
-    InitialSongs();
+      InitialSongs();
+    }
 
   }, []);
 
@@ -66,16 +70,11 @@ const Loadsongs = () => {
     <header className="App-header">
       <h1>Load songs</h1>
 
-      {!token ? (
-        <div>normal page content</div>
-      ) : (
-        <div>search page content</div>
-      )}
+      {!token ? <div>normal page content</div> : <div>search page content</div>}
 
       {renderSongs()}
-
     </header>
   );
-}
+};
 
 export default Loadsongs;
