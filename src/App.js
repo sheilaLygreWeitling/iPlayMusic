d005e82853b1dce1038da7ff2f39cba065734367
MarcusHjorth <U773559@rts-365.dk>
2022-05-16 09:17:37 +0200

    App rettelser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-
 import { Routes, Route } from "react-router-dom"
-
 import HomePage from "./pages/HomePage";
-
-import { Routes, Route } from "react-router-dom";
-
 import NowPlaying from "./pages/Heading/NowPlaying";
-import Home from "./pages/Home";
 
 
 
@@ -15,11 +9,10 @@ function App() {
   return (
 
     <div className="w-full h-screen overflow-hidden bg-white dark:bg-darkPurple">
-      <div>navbar</div>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/Page1" element={<Page1 />} />
+        {/* <Route path="/Page1" element={} /> */}
         <Route path="NowPlaying" element={<NowPlaying />} />
       </Routes>
 
