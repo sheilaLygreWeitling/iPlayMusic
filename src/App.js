08dd47cf18fc7d381f18af0684169fa739bf1083
Dragonashes <dlsfs@tutanota.de>
2022-05-17 13:20:53 +0200

    rename component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AllSongs from "./pages/AllSongs";
 import MusicPlayer from "./pages/MusicPlayer";
+import Searchcomp from "../src/Components/Searchcomp";
 
 function App() {
   return (
-
 
     <div className="w-full h-screen overflow-hidden bg-white dark:bg-darkPurple">
 
@@ -14,7 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
           <Route path="/allsongs" element={<AllSongs/>} />
         <Route path="NowPlaying" element={<MusicPlayer />} />
-        <Route path="/Search" element={<Search />} />
+        <Route path="/Search" element={<Searchcomp />} />
       </Routes>
 
     </div>
