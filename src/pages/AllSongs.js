80d377558e770ce4f5bdbc1ed27d8bc07bb778e6
ozlemertosun <U2407193@rts-365.dk>
2022-05-17 09:19:32 +0200

    add SongArray to branch

--- src/pages/AllSongs.js
+++ src/pages/AllSongs.js
@@ -1,10 +1,11 @@
 import Songs from "../assets/components/allsongs/Songs";
 
+
 const AllSongs = () => {
   return (
     <>
-      <h1 className="text-[48px] w-screen">All Songs</h1>
-      <section className="flex flex-col">
+      <h1 className="text-[48px] ml-[25px] mt-[47px] mb-[27px] w-screen font-bold text-transparent bg-clip-text bg-gradient-to-r from-gradientRedFrom to-gradientRedTo">All Songs</h1>
+      <section className="flex flex-col gap-4">
         <Songs/>
         <Songs/>
         <Songs/>
