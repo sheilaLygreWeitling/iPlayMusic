4bf2e2d0423dead5a6e400c219e363b2101737e1
Dragonashes <dlsfs@tutanota.de>
2022-05-17 10:18:25 +0200

    rm header

--- src/Components/Searchcomp.js
+++ src/Components/Searchcomp.js
@@ -67,7 +67,6 @@ const Searchcomp = () => {
 
   return (
     <header className="App-header">
-      <h1>Spotify React</h1>
       {!token ? (
         <a
           href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
