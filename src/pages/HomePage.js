b9d6c51c217b39b6b36f6f794f2f29a5027963f1
Dragonashes <dlsfs@tutanota.de>
2022-05-18 12:50:54 +0200

    fix link

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,24 +12,24 @@ const HomePage = () => {
   return (
     <div className="w-full h-screen bg-gradient-to-b dark:from-gradientRedFrom to-gradientRedTo flex flex-col items-center">
       <motion.div className="relative top-[-6rem] left-[-1rem]"
-      animate={{ rotate: [ 0, -30, -5, 0 ], 
-        y: [0, 75, 0], 
-        x: [0, 100, -75, 0], 
+      animate={{ rotate: [ 0, -30, -5, 0 ],
+        y: [0, 75, 0],
+        x: [0, 100, -75, 0],
         scale: [1.1, 1.2, 1.1] }}
       transition={{ repeat: Infinity, duration: 30  }}
       >
       <PulseFigure2 className="w-[20rem] h-[20rem]" />
       </motion.div>
-  
+
       <QuotesComponent/>
 
       <nav className="w-[80%] my-[5rem] text-5xl dark:text-white flex flex-row justify-around">
-        
+
         <div className="border-4 border-black dark:border-white rounded-full p-[1rem] flex justify-center items-center">
           <ToggleDarkmode />
         </div>
-        
-        <Link to="/Page1">
+
+        <Link to="/nowplaying">
             <div className="border-4 border-black  dark:border-white rounded-full p-[1rem] flex justify-center items-center">
                 <BsHeart />
             </div>
