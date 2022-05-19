9af6f296f53a4d66b69068a10133bc54b2b9802d
Dragonashes <dlsfs@tutanota.de>
2022-05-19 11:10:55 +0200

    add test component props

--- src/pages/MusicPlayer.js
+++ src/pages/MusicPlayer.js
@@ -4,12 +4,15 @@ import Header from '../assets/components/header/Header';
 
 import FetchSongs from '../assets/components/musicplayer/FetchSongs';
 
+import ThinText from '../assets/components/musicplayer/ThinText';
+
 const MusicPlayer = () => {
     return (
         <>
             <Header />
             <PlayingNow />
 
+            <ThinText text="Nothing found..." />
             <FetchSongs />
         </>
     );
